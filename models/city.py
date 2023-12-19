6373902cbed5ed3ed52413bff21d45947fa440a7
ElaasriTarik <elaasriya2003@gmail.com>
2023-12-20 00:15:49 +0100

    of coirse it did not work

--- models/city.py
+++ models/city.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 """ City Module for HBNB project """
+from sqlalchemy.ext.declarative import declarative_base
 from models.base_model import BaseModel, Base
 from sqlalchemy.orm import relationship
 from models.place import Place
@@ -11,3 +12,5 @@ class City(BaseModel, Base):
     __tablename__ = "cities"
     state_id = Column(String(60), ForeignKey('states.id'), nullable=False)
     name = Column(String(128), nullable=False)
+    places = relationship("Place", cascade='all, delete, delete-orphan',
+                        backref="cities")
