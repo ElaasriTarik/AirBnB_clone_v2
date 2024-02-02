ddccff4d959d401891cab0c634a83a09dbca5efa
Abdessadek EL AASRI <elaasriya2003@gmail.com>
2024-02-02 02:07:06 +0100

    fab

---
+++ 1-pack_web_static.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""compress files"""
+from fabri.api import local
+import datetime import datetime
+import os.path
+
+
+def do_pack():
+    time = datetime.utcnow()
+    local("mkdir -p versions")
+    save_to = "versions/web_static_{}{}{}{}{}{}.tgz".format(time.year,
+                                                            time.month,
+                                                            time.day,
+                                                            time.hour,
+                                                            time.minute,
+                                                            time.second)
+
+    local(f"tar -czf {save_to} web_static")
+    return save_to
