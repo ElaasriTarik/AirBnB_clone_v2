e13a672f93270dece4878cc1add1fe44566ac768
Abdessadek EL AASRI <elaasriya2003@gmail.com>
2024-02-02 15:06:57 +0100

    task 1

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -1,20 +1,22 @@
 #!/usr/bin/python3
-# compress files
-from fabric.api import local
+# acompress files
+from fabric.context_managers import path
+from fbric.api import local, task
 from datetime import datetime
 
 
 @task
 def do_pack():
-    """ compress to a tar gzip file """
-    time = datetime.utcnow()
-    local("mkdir -p versions")
-    save_to = "versions/web_static_{}{}{}{}{}{}.tgz".format(time.year,
-                                                            time.month,
-                                                            time.day,
-                                                            time.hour,
-                                                            time.minute,
-                                                            time.second)
+	""" compress to a tar gzip file """
+	time = datetime.utcnow()
+	local("mkdir -p versions")
+	save_to = "versions/web_static_{}{}{}{}{}{}.tgz".format(time.year,
+								time.month,
+								time.day,
+								time.hour,
+								time.minute,
+								time.second)
 
-    local("tar -czf {} web_static".format(save_to))
-    return save_to
+	with path("web_static"):
+		local("tar -czf {} .".format(save_to))
+	return save_to
