336223842e524801b20057f17a6a490ec2ecf91d
Abdessadek EL aasri <elaasriya2003@gmail.com>
2024-02-15 20:38:31 +0000

    v

---
+++ 2-do_deploy_web_static.py
@@ -0,0 +1,31 @@
+#!/usv/env/python3
+# deploy to my web server
+from fabric import task
+from fabric import Connection, SerialGroup
+import os
+from fabric.api import local, put, run, env
+
+
+env.user = 'ubuntu'
+env.hosts = ['54.157.137.104', '54.84.181.41']
+@task
+def do_deply(archive_path):
+    if not os.path.exists(archive_path):
+        return False
+
+    put(archive_path, '/tmp/')
+
+    #extracting...
+    filename = os.path.basename(archive_path)
+    folder_name = filename.split('.')[0]
+    remote_path = '/data/web_static/releases/'
+
+    conn.run('mkdir -p {}{}/'.format(remote_path, folder_name))
+    conn.run('tar -xzf /tmp/{} -C {}{}/'.format(filename, remote_path, folder_name))
+
+    run('rm /tmp/{}'.format(filename), warn=True)
+    run('rm -rf {}'.format('/data/web_static/current/'))
+    run('ln -s {} {}'.format('/data/web_static/current/', remote_path))
+    print('New version deployed!')
+
+    return True
