b1158e4002e7cb1f01ef9ddbe263efa7a4e23a3f
Abdessadek EL aasri <elaasriya2003@gmail.com>
2024-02-15 14:10:20 +0000

    v

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -7,16 +7,17 @@ from datetime import datetime
 
 @task
 def do_pack():
-	""" compress to a tar gzip file """
-	time = datetime.utcnow()
-	local("mkdir -p versions")
-	save_to = "versions/web_static_{}{}{}{}{}{}.tgz".format(time.year,
-								time.month,
-								time.day,
-								time.hour,
-								time.minute,
-								time.second)
+    """ compress to a tar gzip file """
+    time = datetime.utcnow()
+    local("mkdir -p versions")
+    save_to = "versions/web_static_{}{}{}{}{}{}.tgz".format(
+        time.year,
+        time.month,
+        time.day,
+        time.hour,
+        time.minute,
+        time.second)
 
-	with path("web_static"):
-		local("tar -czf {} web_static".format(save_to))
-	return save_to
+    with path("web_static"):
+        local("tar -czf {} web_static".format(save_to))
+    return save_to
