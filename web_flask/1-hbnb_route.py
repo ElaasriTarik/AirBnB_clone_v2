742d87d7a9f6a75f188605209f6c1be711ec7efa
Abdessadek EL AASRI <elaasriya2003@gmail.com>
2024-02-16 20:49:23 +0100

    f

---
+++ web_flask/1-hbnb_route.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""tarek"""
+from flask import Flask
+
+
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def hello_HBNB():
+    """hello hbnb"""
+    return 'Hello HBNB!'
+
+@app.route('/hbnb', strict_slashes=False)
+def HBNB():
+    """hello hbnb"""
+    return 'HBNB!'
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
