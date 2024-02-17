052e94276d3806877fa2e4036ae3317770d62717
Abdessadek EL AASRI <elaasriya2003@gmail.com>
2024-02-17 20:01:25 +0100

    f

---
+++ web_flask/4-number_route.py
@@ -0,0 +1,55 @@
+#!/usr/bin/python3
+"""tarek"""
+from flask import Flask
+from markupsafe import escape
+
+
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def hello_HBNB():
+    """hello hbnb"""
+    return 'Hello HBNB!'
+
+
+@app.route('/hbnb', strict_slashes=False)
+def HBNB():
+    """hello hbnb"""
+    return 'HBNB'
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def display_text(text):
+    """hello hbnb"""
+    val = 'C '
+    for c in text:
+        if c != '_':
+            val += c
+        else:
+            val += ' '
+    return val
+
+
+@app.route("/python", strict_slashes=False)
+@app.route('/python/<text>', strict_slashes=False)
+def display_py_text(text='is_cool'):
+    """hello hbnb"""
+    val = 'Python '
+    for c in text:
+        if c != '_':
+            val += c
+        else:
+            val += ' '
+    return val
+
+
+@app.route('/number/<n>', strict_slashes=False)
+def display_number(text):
+    """hello hbnb"""
+    if type(n, int):
+        return 'n is a number'
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
